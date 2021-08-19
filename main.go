baba416ab1c42a7f2636541d7b5195b9997eba80
Ryan Larkin <rlarkin212@gmail.com>
2021-08-19 18:13:32 +0100

    test

--- main.go
+++ main.go
@@ -2,14 +2,9 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 	"os"
-	"time"
 
-	tgbot "github.com/go-telegram-bot-api/telegram-bot-api"
-	"github.com/rlarkin212/gw2-tp-notifer/gw2service"
-	"github.com/rlarkin212/gw2-tp-notifer/telegramservice"
 	"github.com/rlarkin212/gw2-tp-notifer/util"
 )
 
@@ -20,30 +15,23 @@ const (
 var tgApi = util.GetEnvVar("TgApiKey")
 
 func main() {
-	bot, err := tgbot.NewBotAPI(tgApi)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
+	// bot, err := tgbot.NewBotAPI(tgApi)
+	// if err != nil {
+	// 	log.Fatal(err.Error())
+	// }
 
 	port := httpPort()
 	http.HandleFunc("/", home)
 	http.ListenAndServe(port, nil)
 
-	for range time.Tick(time.Minute * 6) {
-		getSales(bot)
-		log.Println("called GetSales")
-	}
+	// for range time.Tick(time.Minute * 6) {
+	// 	getSales(bot)
+	// 	log.Println("called GetSales")
+	// }
 }
 
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("yeet"))
-}
-
-func getSales(bot *tgbot.BotAPI) {
-	sales := gw2service.FetchSales(gw2ApiBaseUrl)
-	items := gw2service.FetchItems(gw2ApiBaseUrl, sales)
-
-	telegramservice.SendMessage(bot, items)
 }
 
 func httpPort() string {
@@ -54,3 +42,10 @@ func httpPort() string {
 	}
 	return fmt.Sprintf(":%s", port)
 }
+
+// func getSales(bot *tgbot.BotAPI) {
+// 	sales := gw2service.FetchSales(gw2ApiBaseUrl)
+// 	items := gw2service.FetchItems(gw2ApiBaseUrl, sales)
+
+// 	telegramservice.SendMessage(bot, items)
+// }
