fdc012f8abf733f3ef016fb86073df913db148d0
Ryan Larkin <rlarkin212@gmail.com>
2021-08-19 18:58:23 +0100

    put http back, might have to figure out something different to ping it

--- main.go
+++ main.go
@@ -3,12 +3,16 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
+	"os"
 	"time"
 
 	tgbot "github.com/go-telegram-bot-api/telegram-bot-api"
 	//"github.com/rlarkin212/gw2-tp-notifer/gw2service"
 	//"github.com/rlarkin212/gw2-tp-notifer/telegramservice"
 
+	"github.com/rlarkin212/gw2-tp-notifer/gw2service"
+	"github.com/rlarkin212/gw2-tp-notifer/telegramservice"
 	"github.com/rlarkin212/gw2-tp-notifer/util"
 )
 
@@ -25,15 +29,32 @@ func main() {
 		log.Fatal(err.Error())
 	}
 
+	port := httpPort()
+	http.HandleFunc("/", home)
+	http.ListenAndServe(port, nil)
+
 	for range time.Tick(time.Second * 6) {
 		getSales(bot)
 		fmt.Printf("called GetSales @ %s", time.Now().UTC().Format(iso8601))
 	}
 }
 
-func getSales(bot *tgbot.BotAPI) {
-	// sales := gw2service.FetchSales(gw2ApiBaseUrl)
-	// items := gw2service.FetchItems(gw2ApiBaseUrl, sales)
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("yeet"))
+}
 
-	// telegramservice.SendMessage(bot, items)
+func httpPort() string {
+	port := "5000"
+
+	if os.Getenv("PORT") != "" {
+		port = os.Getenv("PORT")
+	}
+	return fmt.Sprintf(":%s", port)
+}
+
+func getSales(bot *tgbot.BotAPI) {
+	sales := gw2service.FetchSales(gw2ApiBaseUrl)
+	items := gw2service.FetchItems(gw2ApiBaseUrl, sales)
+
+	telegramservice.SendMessage(bot, items)
 }
